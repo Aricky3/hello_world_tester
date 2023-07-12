317665a501344b5b720afe27980abef372531d15
Ricky Aguilar <r3aguila@ucsd.edu>
2023-07-11 17:30:48 -0700

    Initial Commit

---
+++ hello_world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello_world
+//
+//  Created by Ricardo Aguilar on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
